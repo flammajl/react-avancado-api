7761c462e4af7404bc3391d78e45d6a922cd9418
João Luiz <jleaopitta@gmail.com>
2021-07-12 18:57:12 -0300

    feat: initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b1586b9cc4aa08ad38c0af366905edd7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bb41fd76-0a8a-49bd-887b-22e831d1aff7'
+};
